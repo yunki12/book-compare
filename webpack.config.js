95cdbca751f8779414c44ad92d152e238ed47111
yoongibum <yunki1212@nate.com>
2024-10-05 12:54:45 +0900

    pacakage.json webpack에 맞춰서 변경

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+      bundle: path.resolve(__dirname, 'src/index.tsx'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                //exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/i, // CSS 파일을 처리하기 위한 규칙 추가
+                use: ['style-loader', 'css-loader'], // 'style-loader'와 'css-loader'를 사용
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // 템플릿 파일 경로
+            filename: 'index.html', // 생성할 파일 이름
+        }),
+        new CleanWebpackPlugin(),
+    ],
+};
