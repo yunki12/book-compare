73e5f611c86c73fbcbb9d5adc0dcd669631ff8b8
yunki12 <yunki1212@nate.com>
2024-10-07 08:11:26 +0900

    response data 처리

--- src/api/BookApi.tsx
+++ src/api/BookApi.tsx
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
+interface Book {
+    title: string;
+    price: number;
+    reviews: number;
+    rating: string;
+}
+
 // 도서 정보를 가져오는 API 함수
 export const fetchBooks = async (query) => {
     try {
@@ -25,6 +32,17 @@ export const fetchBooks = async (query) => {
         console.log('data' + response.data)
         console.log(Object.keys(response.data).length)
         console.log('data - ' + JSON.stringify(response.data))
+        console.log('data - ' + JSON.stringify(response.data.data))
+
+        // "data" 키로부터 배열을 꺼내서 Book 형태로 변환
+        const books: Book[] = response.data.map((item: string[]) => ({
+            title: item[0],
+            price: Number(item[1].replace(/,/g, "")),  // 가격에서 콤마 제거 후 숫자로 변환
+            reviews: Number(item[2]),  // 리뷰 수를 숫자로 변환
+            rating: item[3],  // 평점은 문자열 그대로 사용
+        }));
+
+        console.log(books);
 
         return response.data;
     } catch (error) {
