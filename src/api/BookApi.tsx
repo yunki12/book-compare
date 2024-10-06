8b43bb321140dee683d3508d4f768587af819c06
yoongibum <yunki1212@nate.com>
2024-10-06 10:00:17 +0900

    도서 리스트 api 요청 처리를 위한 axios package 추가 및 api 분리

---
+++ src/api/BookApi.tsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+// 도서 정보를 가져오는 API 함수
+export const fetchBooks = async (query) => {
+    try {
+/*        const response = await axios.get('https://api.example.com/books', {
+            params: { query },
+        });*/
+
+
+        const data = [
+            {title: '책 이름 1', price: 20000, reviews: 120, rating: '★★★★☆'},
+            {title: '책 이름 2', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 3', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 4', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 5', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 6', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 7', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 8', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 9', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 10', price: 15000, reviews: 80, rating: '★★★★★'},
+        ];
+
+        console.log('data' + data)
+
+        return data;
+    } catch (error) {
+        console.error("API Error: ", error);
+        throw error;
+    }
+};
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
 import '../styles/css/App.css';
 import BookList from '../components/BookList';
+import {fetchBooks} from "../api/BookApi";
+import { useEffect, useState } from 'react';
 
-const App: React.FC = () => {
-    const books = [
-        {title: '책 이름 1', price: 20000, reviews: 120, rating: '★★★★☆'},
-        {title: '책 이름 2', price: 15000, reviews: 80, rating: '★★★★★'},
-        {title: '책 이름 3', price: 15000, reviews: 80, rating: '★★★★★'},
-        {title: '책 이름 4', price: 15000, reviews: 80, rating: '★★★★★'},
-        {title: '책 이름 5', price: 15000, reviews: 80, rating: '★★★★★'},
-        {title: '책 이름 6', price: 15000, reviews: 80, rating: '★★★★★'},
-        {title: '책 이름 7', price: 15000, reviews: 80, rating: '★★★★★'},
-        {title: '책 이름 8', price: 15000, reviews: 80, rating: '★★★★★'},
-        {title: '책 이름 9', price: 15000, reviews: 80, rating: '★★★★★'},
-        {title: '책 이름 10', price: 15000, reviews: 80, rating: '★★★★★'},
-    ];
+const App = () => {
+    const [books, setBooks] = useState([]);
+    const [query, setQuery] = useState('');
+
+    useEffect(() => {
+
+        console.log('query : ' + query)
+        console.log('fetchBooks')
+
+        const fetchData = async () => {
+            try {
+                const result = await fetchBooks(query);
+                setBooks(result);
+            } catch (error) {
+                console.error("Error fetching books: ", error);
+            }
+        };
+        fetchData();
+    }, [query]);
+
+    /*    const books = [
+            {title: '책 이름 1', price: 20000, reviews: 120, rating: '★★★★☆'},
+            {title: '책 이름 2', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 3', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 4', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 5', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 6', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 7', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 8', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 9', price: 15000, reviews: 80, rating: '★★★★★'},
+            {title: '책 이름 10', price: 15000, reviews: 80, rating: '★★★★★'},
+        ];*/
+
+    //const books = await fetchBooks('');
+
+    console.log('메인 페이지')
 
     return (
         <div>
             <BookList books={books}/>
         </div>
     );
-};
+}
 
 export default App;
