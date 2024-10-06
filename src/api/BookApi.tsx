4623a1dc811fad4c65ef103a14850e780806d6a7
yoongibum <yunki1212@nate.com>
2024-10-06 22:09:20 +0900

    반복문 처리

--- src/api/BookApi.tsx
+++ src/api/BookApi.tsx
@@ -3,27 +3,30 @@ import axios from 'axios';
 // 도서 정보를 가져오는 API 함수
 export const fetchBooks = async (query) => {
     try {
-/*        const response = await axios.get('https://api.example.com/books', {
-            params: { query },
-        });*/
+        const response = await axios.get('http://localhost:8000/books/', {
+            withCredentials: true,
+            params: {query},
+        });
 
 
-        const data = [
-            {title: '책 이름 1', price: 20000, reviews: 120, rating: '★★★★☆'},
-            {title: '책 이름 2', price: 15000, reviews: 80, rating: '★★★★★'},
-            {title: '책 이름 3', price: 15000, reviews: 80, rating: '★★★★★'},
-            {title: '책 이름 4', price: 15000, reviews: 80, rating: '★★★★★'},
-            {title: '책 이름 5', price: 15000, reviews: 80, rating: '★★★★★'},
-            {title: '책 이름 6', price: 15000, reviews: 80, rating: '★★★★★'},
-            {title: '책 이름 7', price: 15000, reviews: 80, rating: '★★★★★'},
-            {title: '책 이름 8', price: 15000, reviews: 80, rating: '★★★★★'},
-            {title: '책 이름 9', price: 15000, reviews: 80, rating: '★★★★★'},
-            {title: '책 이름 10', price: 15000, reviews: 80, rating: '★★★★★'},
-        ];
+        /*        const data = [
+                    {title: '책 이름 1', price: 20000, reviews: 120, rating: '★★★★☆'},
+                    {title: '책 이름 2', price: 15000, reviews: 80, rating: '★★★★★'},
+                    {title: '책 이름 3', price: 15000, reviews: 80, rating: '★★★★★'},
+                    {title: '책 이름 4', price: 15000, reviews: 80, rating: '★★★★★'},
+                    {title: '책 이름 5', price: 15000, reviews: 80, rating: '★★★★★'},
+                    {title: '책 이름 6', price: 15000, reviews: 80, rating: '★★★★★'},
+                    {title: '책 이름 7', price: 15000, reviews: 80, rating: '★★★★★'},
+                    {title: '책 이름 8', price: 15000, reviews: 80, rating: '★★★★★'},
+                    {title: '책 이름 9', price: 15000, reviews: 80, rating: '★★★★★'},
+                    {title: '책 이름 10', price: 15000, reviews: 80, rating: '★★★★★'},
+                ];*/
 
-        console.log('data' + data)
+        console.log('data' + response.data)
+        console.log(Object.keys(response.data).length)
+        console.log('data - ' + JSON.stringify(response.data))
 
-        return data;
+        return response.data;
     } catch (error) {
         console.error("API Error: ", error);
         throw error;
