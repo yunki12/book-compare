6599c53d502b09266a76fb1d2f3b93ff51a6fd2a
yoongibum <yunki1212@nate.com>
2025-03-20 19:31:15 +0900

    App Book List 가져오는 코드 불필요 해졌으므로 제거

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -12,22 +12,6 @@ import NavigationBar from "../components/NavigationBar";
 import Router from '../components/AppRoutes'; // Router 컴포넌트 임포트
 
 const App = () => {
-    const [books, setBooks] = useState([]);
-    const [query, setQuery] = useState('');
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const result = await fetchBooks(query);
-                setBooks(result);
-            } catch (error) {
-                console.error("Error fetching books: ", error);
-            }
-        };
-        fetchData()
-            .then()
-            .catch((error) => console.error("Error in fetchData: ", error));
-    }, [query]);
     return (
         <div>
             <NavigationBar />
